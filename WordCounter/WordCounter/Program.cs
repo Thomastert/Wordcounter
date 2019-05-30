b0ffee16ec1474b8551ce74370175cc47cb6a927
Thomastert <thomas@thoemda.com>
2019-05-30 16:51:39 +0200

    gitPush

---
+++ WordCounter/WordCounter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace WordCounter
+{
+    class Program
+    {
+        //Counts the number of individual words in a string.
+        //For added complexity read these strings in from a text file and generate a summary.
+        static void Main(string[] args)
+        {
+            string userImput = File.ReadAllText(@"D:\c#practis\rapidfire\StringWordCounter\WordList.txt");
+            var wordList = 1;
+
+            for (int i = 0; i < userImput.Length; i++)
+            {
+                if (userImput[i] == ' ')
+                {
+                    wordList++;
+                }
+            }
+            Console.WriteLine(wordList);
+
+
+            Console.WriteLine("end");
+            Console.ReadKey();
+        }
+    }
+}
